6d1ab91d5f62c3a6138d7f8596b638ec46ef50aa
Lachlan Astfalck <astfalckl@gmail.com>
2021-07-16 09:05:08 +0800

    Added sst_sim class and PMIP sst data

---
+++ R/data_documentation.R
@@ -0,0 +1,7 @@
+#' PMIP SST Simulations
+#' 
+#' A collection of SST Simulations from PMIP 3 and 4.
+#' 
+#' @format A sst_sim object of PMIP outputs
+#' @source Go through and ref every object in here
+"pmip_sst"
---
+++ R/sst_sim_class.R
@@ -0,0 +1,39 @@
+
+#' Crate a sst_sim object
+#' 
+#' This creates the object that holds the sst_sim results, i.e., the X
+#'
+#' @param object List with elements XYZ
+#'
+#' @return Returns a sst_sim object
+#' @export
+sst_sim <- function(object){
+	
+	obj_names <- names(object)
+
+  new_sst_sim(object)
+
+}
+
+#' Helper function to assign class to a sst_sim object
+#'
+#' @param object sst_sim object from sst_sim()
+#'
+#' @return Returns a sst_sim object
+#' @export
+new_sst_sim <- function(object){
+  structure(
+    object,
+    class = "sst_sim"
+  )
+}
+
+#' Print function for sst_sim object
+#'
+#' @param object sst_sim object
+#'
+#' @return Prints sst_sim object
+#' @export
+print.sst_sim <- function(object){
+  utils::str(object)
+}
