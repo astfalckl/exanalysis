6be9c23c5b0749d8ff68f5cae0352a7886180124
Lachlan Astfalck <astfalckl@gmail.com>
2021-10-27 17:00:13 +0800

    Fixed sst inversion problem

--- R/update_sst.R
+++ R/update_sst.R
@@ -33,19 +33,22 @@ calculate_sst_update <- function(simulation, proxy, H_list, params, incl_varM = 
 
 	if(incl_varM){varMs <- calculate_Ms(simulation)}
 
-	varYS <- varS + M_scale * varM
+	mask <- as.matrix(varS)
+	mask[which(as.matrix(varS) != 0)] <- 1
+
+	varYS <- varS + M_scale/2 * Matrix(varMs) #Matrix(mask * varMs)
 
 	one <- matrix(1/simulation$ntime, nrow = simulation$ntime, ncol = 1)
 
 	cat(sprintf("\rCalculating H components..."))
 
-	HvarY <- H_list$Hs %*% kronecker(rbind(varYS, varYS), t(one) %*% varT)
+	# HvarY <- H_list$Hs %*% rbind(varYS, varYS)
+	HsvarYs <- H_list$Hs %*% rbind(varYS, varYS)
+	HvarY <- kronecker(HsvarYs, t(one) %*% varT)
 
 	HvarYH <- H_list$Hs %*% 
 		kronecker(matrix(1, nrow = 2, ncol = 2), varYS) %*% 
 		Matrix::t(H_list$Hs)
-
-	HsvarYs <- H_list$Hs %*% rbind(varYS, varYS)
 
 	solveU <- solve(HvarYH + proxy$varB + D)
 
@@ -55,10 +58,19 @@ calculate_sst_update <- function(simulation, proxy, H_list, params, incl_varM = 
 
 	cat(sprintf("\rCalculating updates...     "))
 
-	E <- M + Matrix::t(HvarY) %*% solveU %*% as.matrix(error)
+	E <- M + Matrix::t(HvarY1) %*% solveU %*% as.matrix(error)
 
 	Vs <- varYS - Matrix::t(HsvarYs) %*% solveU %*% HsvarYs
 	Vt <- varT
+
+	update_tbl <- sst_update$simulation$means %>%
+		rename(Eadj1 = sst) %>%
+		mutate(
+			Vadj1 = rep(diag(varYS), each = 12),
+			Eadj2 = as.numeric(E),
+			Eadj2 = ifelse(Eadj2 < -1.92, -1.92, Eadj2),
+			Vadj2 = rep(diag(Vs), each = 12)
+		)
 
 	return(
 		sst_reconstruction(
@@ -67,7 +79,8 @@ calculate_sst_update <- function(simulation, proxy, H_list, params, incl_varM = 
 				proxy = proxy,
 				E = E,
 				Vs = Vs,
-				Vt = Vt
+				Vt = Vt,
+				update_tbl = update_tbl
 			)
 		)
 	)
